419931443151cf7a64ab07bb30abbe67d41a48bd
celsiusqc <zachary-boland@outlook.com>
2022-12-24 00:57:25 -0500

    1.19.2 First build

---
+++ src/main/java/net/mcreator/createaddon/item/BrassDrillItem.java
@@ -0,0 +1,38 @@
+
+package net.mcreator.createaddon.item;
+
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.CreativeModeTab;
+
+public class BrassDrillItem extends PickaxeItem {
+	public BrassDrillItem() {
+		super(new Tier() {
+			public int getUses() {
+				return 1000;
+			}
+
+			public float getSpeed() {
+				return 6f;
+			}
+
+			public float getAttackDamageBonus() {
+				return 0f;
+			}
+
+			public int getLevel() {
+				return 2;
+			}
+
+			public int getEnchantmentValue() {
+				return 0;
+			}
+
+			public Ingredient getRepairIngredient() {
+				return Ingredient.EMPTY;
+			}
+		}, 1, -3.2f, new Item.Properties().tab(CreativeModeTab.TAB_TOOLS));
+	}
+}
